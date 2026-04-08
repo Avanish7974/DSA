839a325bc44f8f37d94177d5682d520a133717a3
Avanish7974 <avanishsingh4318@gmail.com>
2026-04-09 01:31:18 +0530

    vector

---
+++ Vector/vector.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main () {
+    vector<int> vec = {1, 2, 3, 4, 5};
+    cout<<"Vector elements: ";
+    cout<<vec[0];
+
+
+    return 0;
+}
