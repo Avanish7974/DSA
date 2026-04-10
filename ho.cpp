5055facc2b80ed0d779e4e8be60b054ebb0e5556
Avanish7974 <avanishsingh4318@gmail.com>
2026-04-10 13:16:59 +0530

    chamka

--- ho.cpp
+++ ho.cpp
@@ -12,29 +12,3 @@ int main() {
     cout<<"Value of b: "<<b<<endl;
     return 0;
 }
-class Solution {
-public:
-    int searchInsert(vector<int>& nums, int target) {
-
-        int start = 0;
-        int end = nums.size() - 1;
-        int ans;
-
-        while(start <= end) {
-            int mid = (start + end) / 2;
-
-            if(nums[mid] == target)
-                return mid;
-
-            else if(nums[mid] < target)
-                start = mid + 1;
-
-            else{
-                ans = mid;
-                end = mid - 1;
-            }
-        }
-
-        return ans;
-    }
-};
