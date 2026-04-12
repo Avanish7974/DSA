ff3e5f494a905a7e0d6b4a20c17ea3e90f2cc1a0
Avanish7974 <avanishsingh4318@gmail.com>
2026-04-12 20:34:57 +0530

    product

--- ho.cpp
+++ ho.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 using namespace std;
 int main() {
-    
-    int a = 10;
-    int b = 20;
+    int n = 5;
+    int arr[n] = {1, 2, 3, 4, 5};
 
-    int a = a^b; // a now holds the result of a XOR b
-    int b = a^b; // b now holds the original value of a
-    int a = a^b; // a now holds the original value of b
-    cout<<"Value of a: "<<a<<endl;
-    cout<<"Value of b: "<<b<<endl;
+    for(int i = 0;i<n;i++) {
+        int multiply = 1;
+        for(int j = 0;j<n;j++) {
+            multiply *= arr[j];
+        }
+        cout<<"Product of elements in row "<<i+1<<": "<<multiply<<endl;
+    }
+    
+
+    
+    
     return 0;
 }
+// int a = 10;
+    // int b = 20;
+
+    // int a = a^b; // a now holds the result of a XOR b
+    // int b = a^b; // b now holds the original value of a
+    // int a = a^b; // a now holds the original value of b
+    // cout<<"Value of a: "<<a<<endl;
+    // cout<<"Value of b: "<<b<<endl;
