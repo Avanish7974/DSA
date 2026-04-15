988f9096394e343ffd626535e01b1e2a16e67ae7
Avanish7974 <avanishsingh4318@gmail.com>
2026-04-15 20:48:52 +0530

    fre

--- practice/first.cpp
+++ practice/first.cpp
@@ -4,17 +4,30 @@ using namespace std;
 int main() {
     int n = 8;
     int arr[n] = {1,3,2,3,4,1,5,2};
-    for(int i = 0;i<n;i++) {
-        int sum = 1;
-        for(int j = i+1;j<n;j++) {
-            if(arr[i] == arr[j]) {
-                sum++;
-            } 
-              
+
+    for(int i = 0; i < n; i++) {
+
+        bool visited = false;
+
+        // check if element already counted
+        for(int k = 0; k < i; k++) {
+            if(arr[i] == arr[k]) {
+                visited = true;
+                break;
+            }
         }
-        
-        cout<<"Frequency of "<<arr[i]<<" is: "<<sum<<endl;
-        
+
+        if(visited) continue;
+
+        int count = 1;
+
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+            }
+        }
+
+        cout << "Frequency of " << arr[i] << " is: " << count << endl;
     }
 
     return 0;
