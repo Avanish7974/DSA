49a4f5a19cac8f1c3d24c636556cd6d71e8bfd47
Avanish7974 <avanishsingh4318@gmail.com>
2026-05-06 21:40:48 +0530

    swap

--- hlf.cpp
+++ hlf.cpp
@@ -20,10 +20,10 @@ int main() {
 
     // }
     int a = 10,b = 20;
-    a = a^b;
-    b = a^b;
-    a= a^b;
+    a = a+b;
+    b = a-b;
+    a = a-b;
     cout<<a<<" " <<b;
-    
+
     return 0;
 }
