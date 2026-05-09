422d610585bb6532250dc3f244bcab7ea2baa842
Avanish7974 <avanishsingh4318@gmail.com>
2026-05-10 03:18:08 +0530

    Radha

--- String/palindrome.cpp
+++ String/palindrome.cpp
@@ -13,15 +13,22 @@ int main() {
     int first = 0;
     int last = str.length() - 1;
 
+    // Compare characters from both ends
     while(first < last)
     {
+        // If characters are not same
+        // then string is not palindrome
         if(str[first] != str[last])
         {
             cout << "Not Palindrome" << endl;
+
             return 0;
         }
 
+        // Move left pointer forward
         first++;
+
+        // Move right pointer backward
         last--;
     }
 
@@ -34,10 +41,12 @@ int main() {
 
     string str2 = "Hello Baby";
 
+    // Initially one word is present
     int count = 1;
 
     for(int i = 0; i < str2.length(); i++)
     {
+        // Every space means new word
         if(str2[i] == ' ')
         {
             count++;
@@ -47,24 +56,29 @@ int main() {
     cout << "Number of words in the string: " << count << endl << endl;
 
     // ---------------------------------------------------
-    // Code for finding the length of the longest word in a string
+    // Code for finding the length of the longest word
 
     cout << "Code for finding the length of the longest word in a string" << endl;
 
     string str3 = "Hello Baby kaise ho";
 
-    int len = 0, maxLen = 0;
+    int len = 0;
+    int maxLen = 0;
 
     for(int i = 0; i < str3.length(); i++)
     {
+        // If space comes
+        // new word starts
         if(str3[i] == ' ')
         {
             len = 0;
         }
         else
         {
+            // Increase current word length
             len++;
 
+            // Store maximum length
             if(len > maxLen)
             {
                 maxLen = len;
@@ -72,28 +86,40 @@ int main() {
         }
     }
 
-    cout << "Length of the longest word in the string: " << maxLen << endl << endl;
+    cout << "Length of the longest word in the string: "
+         << maxLen << endl << endl;
 
     // ---------------------------------------------------
     // Code for counting vowels, consonants and spaces
 
     cout << "Code for counting vowels, consonants and spaces" << endl;
 
-    string str4 = "College aa bhi aa jaya karo kabhi kabhi apne jalwe bikharne";
+    string str4 =
+    "College aa bhi aa jaya karo kabhi kabhi apne jalwe bikharne";
 
-    int vowels = 0, consonants = 0, space = 0;
+    int vowels = 0;
+    int consonants = 0;
+    int space = 0;
 
     for(int i = 0; i < str4.length(); i++)
     {
-        if(str4[i] =='a' || str4[i] == 'e' || str4[i] == 'i' || str4[i] == 'o' || str4[i] == 'u' ||
-           str4[i] == 'A' || str4[i] == 'E' || str4[i] == 'I' || str4[i] == 'O' || str4[i] == 'U')
+        // Check vowel
+        if(str4[i] =='a' || str4[i] == 'e' ||
+           str4[i] == 'i' || str4[i] == 'o' ||
+           str4[i] == 'u' || str4[i] == 'A' ||
+           str4[i] == 'E' || str4[i] == 'I' ||
+           str4[i] == 'O' || str4[i] == 'U')
         {
             vowels++;
         }
+
+        // Check space
         else if(str4[i] == ' ')
         {
             space++;
         }
+
+        // Remaining characters are consonants
         else
         {
             consonants++;
@@ -113,11 +139,18 @@ int main() {
 
     for(int i = 0; i < str5.length(); i++)
     {
-        if(str5[i] =='a' || str5[i] == 'e' || str5[i] == 'i' || str5[i] == 'o' || str5[i] == 'u' ||
-           str5[i] == 'A' || str5[i] == 'E' || str5[i] == 'I' || str5[i] == 'O' || str5[i] == 'U')
+        // Check vowel
+        if(str5[i] =='a' || str5[i] == 'e' ||
+           str5[i] == 'i' || str5[i] == 'o' ||
+           str5[i] == 'u' || str5[i] == 'A' ||
+           str5[i] == 'E' || str5[i] == 'I' ||
+           str5[i] == 'O' || str5[i] == 'U')
         {
+            // Remove vowel
             str5.erase(i,1);
 
+            // After erase characters shift left
+            // so check same index again
             i--;
         }
     }
@@ -134,6 +167,7 @@ int main() {
     int first2 = 0;
     int last2 = str6.length() - 1;
 
+    // Swap characters from both ends
     while(first2 < last2)
     {
         swap(str6[first2], str6[last2]);
@@ -142,7 +176,66 @@ int main() {
         last2--;
     }
 
-    cout << str6 << endl;
+    cout << str6 << endl << endl;
+
+    // ---------------------------------------------------
+    // Code for removing spaces from a string
+
+    cout << "Code for removing spaces from a string" << endl;
+
+    string str7 = "Hello babu miss you";
+
+    for(int i = 0; i < str7.length(); i++)
+    {
+        // If space found
+        if(str7[i] == ' ')
+        {
+            // Remove space
+            str7.erase(i,1);
+
+            // Check same index again
+            i--;
+        }
+    }
+
+    cout << str7 << endl << endl;
+
+    // ---------------------------------------------------
+    // Code for finding sum of numbers in a string
+
+    cout << "Code for finding sum of numbers in a string" << endl;
+
+    string str8 = "1xy3z23";
+
+    // current number being formed
+    int sum = 0;
+
+    // final answer
+    int num = 0;
+
+    for(int i = 0; i < str8.length(); i++)
+    {
+        // Check if character is digit
+        if(str8[i] >= '0' && str8[i] <= '9')
+        {
+            // Form number
+            sum = sum * 10 + (str8[i] - '0');
+        }
+        else
+        {
+            // Add current number into final answer
+            num += sum;
+
+            // Reset current number
+            sum = 0;
+        }
+    }
+
+    // Add last remaining number
+    num += sum;
+
+    cout << "Sum of numbers in the string: "
+         << num << endl << endl;
 
     return 0;
 }
