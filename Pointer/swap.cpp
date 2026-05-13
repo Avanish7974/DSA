037f3fa8c5f11ca427e58956352d0b62ab0c0b34
Avanish7974 <avanishsingh4318@gmail.com>
2026-05-14 01:48:07 +0530

    reference

--- Pointer/swap.cpp
+++ Pointer/swap.cpp
@@ -8,39 +8,45 @@ using namespace std;
 //     cout<<endl;
 //     cout<<v;
 // }
+void value(int a,int b){
+    int temp = a;
+    a = b;
+    b = temp;
+    // cout<<"After swap: "<<a<<" "<<b<<endl;
+}
+void pointer(int *a,int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    // cout<<"After swap: "<<*a<<" "<<*b<<endl;
+}
+void reference(int &a1,int &b1){
+    int temp = a1;
+    a1 = b1;
+    b1 = temp;
+    // cout<<"After swap: "<<a1<<" "<<b1<<endl;
+}
 int main() {
-    // int a =10;
-    // int b = 20;
-    // int c = 30;
-    // cout<<&a;
-    // cout<<endl;
-    // cout<<&b;
-    // cout<<endl;
-    // int *p1 = &a;
-    // cout<<p1;
-    // cout<<endl;
-    // cout<<*p1;
-    // int temp = *p1;
-    // cout<<endl;
-    // cout<<temp;
-    // cout<<"sss";
-    // cout<<endl;
+    int x = 10;
+    int y = 20;
+    cout<<"Before swap: "<<x<<" "<<y<<endl;
+    value(x,y);
+    cout<<"Value Swap"<<endl;
+    cout<<"After swap: "<<x<<" "<<y<<endl;
+    pointer(&x,&y);
+    cout<<"Pointer Swap"<<endl;
+    cout<<"After swap: "<<x<<" "<<y<<endl;
+    reference(x,y); 
+    cout<<"Reference Swap"<<endl;
+    cout<<"After swap: "<<x<<" "<<y<<endl;
 
-    // pointer(&a,&b);
-
-    // int *p2 = &c;
-    // cout<<p2;
-    // cout<<endl;
-    // cout<<*p2;
-    int a = 10;
-    int b = 20;
-    int *p1 = &a;
-    cout<<p1;
-    cout<<endl;
-    *p1 = b;
-    cout<<*p1;
-    cout<<endl;
-    cout<<a;
+    int c = 10;
+    int d = 20;
+    int &e = c; // e is a reference to c
+    cout<<e<<endl; // Output: 10
+    e = 30; // Modifying e will modify c
+    cout<<c<<endl; // Output: 30
+    
 
    
 
