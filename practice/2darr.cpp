d753fc3469965a188301755e41b5d831c05a5756
Avanish7974 <avanishsingh4318@gmail.com>
2026-05-16 17:10:53 +0530

    string

--- practice/2darr.cpp
+++ practice/2darr.cpp
@@ -1,32 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
-// class Solution {
-// public:
-//     int diagonalSum(vector<vector<int>>& mat) {
-//     int n = mat.size();
-//     int sum = 0;
-//     int first = 0;
-//     int last = n - 1;
-//     while(first < n){
-//         sum +=  mat[first][last];
-//         first++;
-//         last--;
-//     }
-//     int fir = 0;
-//     int las = 0;
-//     while(fir < n){
-//         sum +=  mat[fir][las];
-//         fir++;
-//         las++;
-//     }
-//     return sum;
-        
-//     }
-// };
+
 int main() {
-    vector<vector<int>> mat = {{1,2,3},{4,5,6},{7,8,9}};
-    Solution s;
-    cout << s.diagonalSum(mat) << endl;
+    string s = "poiinter";
+    string s1 = "";
+    cout << s << endl;
+    int n = s.length();
+    for(int i = 0;i<n;i++) {
+        if(s[i] == 'i'){
+            reverse(s1.begin(), s1.end());
+        }
+        else{
+            s1.push_back(s[i]);
+        }
+    }
+    cout << s1 << endl;
     return 0;
 }
