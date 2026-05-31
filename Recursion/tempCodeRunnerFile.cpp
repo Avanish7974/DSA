f3fc1425fa9cdd768979fe799093be8e41b02c1f
Avanish7974 <avanishsingh4318@gmail.com>
2026-05-31 19:46:39 +0530

    array

--- Recursion/tempCodeRunnerFile.cpp
+++ Recursion/tempCodeRunnerFile.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <climits>
 using namespace std;
 
 int main() {
-    int n = 3;
-    int arr[n] = {1, 2, 3};
-    for (int i = 0; i < n; i++) {
-        int index = i;
-        cout << arr[index] << " ";
-        for (int j = i; j < n-1; j++) {
-            cout << arr[i]<<arr[j+1] << " ";
+    
+    // int arr[n] = {1, 2, 3}; // Example array
+    vector<int> nums = {1,1,1};
+    int k = 3;
+    vector<int> nums = {2,4,3,5};
+
+
+    int count = 0;
+        for(int i = 0;i<nums.size();i++){
+            int sum = 0;
+            for(int j = i;j<nums.size();j++){
+                sum += nums[j];
+                if(sum == k){
+                    count++;
+                }
+            }
+
         }
-    }
+    
+
     return 0;
 }
