dd96bb7465a7c2c49175951badc38a4b54df8e3e
Avanish7974 <avanishsingh4318@gmail.com>
2026-06-04 22:14:11 +0530

    oops

--- Leetcode.cpp
+++ Leetcode.cpp
@@ -6,22 +6,11 @@
 using namespace std;
 
 int main() {
-    vector<int>nums = {-5,  -3, -4};
-    int n = nums.size();
-    sort(nums.begin(), nums.end());
-    long long max = nums[n-1]; 
-    cout << max+1 << endl;
-    if(max < 0){
-        return 1;
-    }
-    for(int i = 1; i <= max+1; i++){
-        if(find(nums.begin(), nums.end(), i) == nums.end()){
-           return i;
-        }
-    }
-    
-
-
+    string s;
+    cin >> s;
+    cout<<s;
+    // getline(cin, s);
+    // cout<<s;
 
     return 0;
 }
