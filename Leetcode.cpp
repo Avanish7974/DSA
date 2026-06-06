e3cc412ec71a86c40b5db14597bce75ce9db1cad
Avanish7974 <avanishsingh4318@gmail.com>
2026-06-06 21:09:29 +0530

    left

--- Leetcode.cpp
+++ Leetcode.cpp
@@ -6,11 +6,44 @@
 using namespace std;
 
 int main() {
-    string s;
-    cin >> s;
-    cout<<s;
-    // getline(cin, s);
-    // cout<<s;
+    int nums[4] = {10,4,8,3};
+    int n = sizeof(nums)/sizeof(nums[0]);
+    vector<int>right;
+    for(int i = 0;i<n-1;i++){
+        int sum  = 0;
+       for(int j = i+1;j<n;j++){ 
+             sum += nums[j];
+       }
+         right.push_back(sum);
+    }
+    right.push_back(0);
+    for(int i = 0;i<n;i++){
+        cout << right[i] << " ";
+    }
+    cout << endl;
+    vector<int>left;
+    // left.push_back(0);
+    for(int i = 0;i<n;i++){
+        int sum  = 0;
+       for(int j = i-1;j>=0;j--){ 
+             sum += nums[j];
+       }
+         left.push_back(sum);
+    }
+    for(int i = 0;i<n;i++){
+        cout << left[i] << " ";
+    }
+    vector<int>answer;
+    for(int i = 0;i<n;i++){
+        answer.push_back(abs(left[i]-right[i]));
+    }
+    cout << endl;
+    for(int i = 0;i<n;i++){
+        cout << answer[i] << " ";
+    }
+
+
+
 
     return 0;
 }
