f83b895cef44c783f1ffc333b0e0d3e5c7c638c1
Avanish7974 <avanishsingh4318@gmail.com>
2026-06-19 23:21:05 +0530

    procure

--- Leetcode.cpp
+++ Leetcode.cpp
@@ -7,29 +7,24 @@ using namespace std;
 
 int main() {
     
-     int arr[10] = {1,2,3,4,5,6};
-     int n = 6;
+    int nums[6] = {1,2,1,3,2,5};
+    vector<int>nu;
 
-     int pos = 2;
-     int value =20;
-     for(int i = 6;i>pos;i--){
-        arr[i] = arr[i-1];
-     }
-     arr[pos] = value;
-     n++;
-     
-     for(int i = 0;i<n;i++){
-        cout<<arr[i]<<" ";
-     }
-     cout<<endl;
 
-     for(int i = pos;i<n-1;i++){
-        arr[i] = arr[i+1];
-     }
-     n--;
-     for(int i = 0;i<n;i++){
-        cout<<arr[i]<<" ";
-     }
+    for (int i = 0; i < nums.size(); i++) {
+        int num = nums[i];
+        int freq = 0;
+
+        for (int j = 0; j < nums.size(); j++) {
+            if (num == nums[j]) {
+                freq++;
+            }
+        }
+        if (freq == 1) {
+            nu.push_back(num);
+        }
+
+    }
 
     return 0;
 }
