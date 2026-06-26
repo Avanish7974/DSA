0fb93f1d54ce9fc23af1bceec8ab7aad83d356c2
Avanish7974 <avanishsingh4318@gmail.com>
2026-06-26 23:24:25 +0530

    recursion

--- Leetcode.cpp
+++ Leetcode.cpp
@@ -7,19 +7,41 @@ using namespace std;
 int main() {
     
     vector<int> nums = {1, 3,4,2,2};
+    vector<int> nums2 = {5,6};
+
+
 
     // Print vector
-    sort(nums.begin(),nums.end());
+    // sort(nums.begin(),nums.end());
+    // for (int i = 0; i < nums.size(); i++) {
+    //     cout << nums[i] << " ";
+    // }
+    // cout<<endl;
+    int k = 0;
+
+    for (int i = 0; i < nums2.size(); i++) {
+        nums.push_back(nums2[i]);
+    }
     for (int i = 0; i < nums.size(); i++) {
         cout << nums[i] << " ";
     }
-    cout<<endl;
+    int n = nums.size();
 
-    for (int i = 0; i < nums.size()-1; i++) {
-        if(nums[i]==nums[i+1]){
-            cout<<nums[i];
-        }
-    }
+if (n % 2 == 0) {
+    cout << "Middle elements: "
+         << nums[n/2 - 1] << " "
+         << nums[n/2];
+}
+else {
+    cout << "Middle element: "
+         << nums[n/2];
+}
+    
+
+
+
+   
+
     
     
 
